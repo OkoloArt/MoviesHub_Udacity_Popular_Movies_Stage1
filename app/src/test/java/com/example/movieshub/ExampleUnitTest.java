a11f2c84e3d345aa45d97b6bc4abdeb807617ffd
Arthur <okoloarthur4@gmail.com>
2021-07-30 17:15:33 +0100

    Udacity Popular Movies Stage1

--- app/src/test/java/com/example/movieshub/ExampleUnitTest.java
+++ app/src/test/java/com/example/movieshub/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.example.movieshub;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
